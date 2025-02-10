da4237880834c0364bd1dd485cad6b490915ebb3
JEMilburn7 <jemilburn7@gmail.com>
2025-02-10 15:31:32 -0600

    Finished Dash
    Added cooldown animation.

--- Assets/Scripts/DashIconBehavior.cs
+++ Assets/Scripts/DashIconBehavior.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DashIconBehavior : MonoBehaviour
 {
     TextMeshProUGUI label;
+    public float fill;
+    Image overlay;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         label = GetComponent<TextMeshProUGUI>();
+        Image[] images = GetComponentsInChildren<Image>();
+        for(int i = 0; i < images.Length; i++) {
+            if(images[i].tag == "overlay") {
+                overlay = images[i];
+            }
+        }
+        fill = 0.0f;
     }
 
     // Update is called once per frame
@@ -17,6 +27,8 @@ public class DashIconBehavior : MonoBehaviour
         string message = "";
         if(PinBehavior.cooldown > 0.0) {
             message = string.Format("{0:0.0}", PinBehavior.cooldown);
+            fill = PinBehavior.cooldown / PinBehavior.cooldownRate;
+            overlay.fillAmount = fill;
         }
         label.text = message;
     }
